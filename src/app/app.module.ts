f18e792aa95d4151d0c9e5044fc1586d24f3ab5c
mo sayed <mo.sayed@anatwine.com>
2017-10-16 07:08:17 +0100

    added navbar and styled home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AppCommonModule} from "./common/common.module";
+import {NavBarComponent} from "./common/nav-bar/nav-bar.component";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppCommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contacts/contacts.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateContactComponent } from './create-contact/create-contact.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CreateContactComponent]
+})
+export class ContactsModule { }
